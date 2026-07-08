1308f040291d964ee19e544b49e242cb998817ed
Cesar <cesarrodrigoricales@gmail.com>
2026-07-08 11:17:35 -0500

    Primer desploy Psicontrol

--- models/index.js
+++ models/index.js
@@ -1,13 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('LocalPsico', 'root', null, {
-  host: '127.0.0.1',
-  dialect: 'mysql',
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3303,
+    dialect: 'mysql',
+    logging: false
+  }
+);
 
 sequelize.authenticate()
-  .then(() => console.log('✅ Conexión a la BD exitosa'))
-  .catch(err => console.error('❌ Error conectando a la BD:', err));
+  .then(() => console.log('Conexión a la BD exitosa'))
+  .catch(err => console.error('Error conectando a la BD:', err));
 
 module.exports = { sequelize };
